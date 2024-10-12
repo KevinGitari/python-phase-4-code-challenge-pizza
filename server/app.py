8dfe2806b4909a98d1e9e8fe9291964817818702
KevinGitari <Kevin>
2024-10-12 12:07:57 +0300

    added the required imports

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from models import db, Restaurant, RestaurantPizza, Pizza
-from flask_migrate import Migrate
-from flask import Flask, request, make_response
-from flask_restful import Api, Resource
 import os
+from flask import Flask, jsonify, make_response
+from flask_migrate import Migrate
+from flask_restful import Api
+from models import db, Restaurant, RestaurantPizza, Pizza
+from routes import _restaurant_routes
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -13,17 +14,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
-
 db.init_app(app)
+migrate = Migrate(app, db)
 
 api = Api(app)
 
+_restaurant_routes(app)
 
 @app.route("/")
 def index():
-    return "<h1>Code challenge</h1>"
+    return "<h1>Code Challenge</h1>"
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
